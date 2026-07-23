dd188dca821ee665f6e842a7ca30ca4482d7b2ce
sneharajmane07@gamil.com <sneharajmane07@email.com>
2026-07-23 14:00:28 +0530

    day2

--- day2/data.tf
+++ day2/data.tf
@@ -1,23 +1,27 @@
-data "aws-vpc" "exisiting_vpc" {
-    exists = true
-    resource "aws_security_group" "sg" {
-        name = "my-security-group"
-        description = "My security group"
-        vpc_id = data.aws_vpc.existing_vpc.id
-        ingress {
-            from_port   = 22
-            to_port     = 22
-            protocol    = "tcp"
-            cidr_blocks = ["0.0.0.0/0"]
-            } 
-    }      
-          egress {
-            from_port   = 0
-            to_port     = 0
-            protocol    = "-1"
-            cidr_blocks = ["0.0.0.0/0"] 
-            }        
-   tags = {
-        Name = "MySecurityGroup"
-    }
-}   
+data "aws_vpc" "existing_vpc" {
+  default = true
+}
+
+resource "aws_security_group" "sg" {
+  name        = "my-security-group"
+  description = "My security group"
+  vpc_id      = data.aws_vpc.existing_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "MySecurityGroup"
+  }
+}
